a438c5eef01ed7504fc1bba6fb77c8961ee5c93f
Estefanía perez <list>
2023-03-16 19:55:24 +0000

    check-error-password

--- src/pages/SignUp.test.js
+++ src/pages/SignUp.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, it, vi} from "vitest";
 import SignUp from "./SignUp.vue";
 import {screen, waitFor} from "@testing-library/dom";
-import { render } from "@testing-library/vue";
+import { cleanup, render } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 
 
@@ -27,3 +27,27 @@ describe("signup", () => {
     });
   });
 });
+
+describe("password", ()=> {
+  it("check error if shortpassword", async () => {
+    const user = userEvent.setup();
+    cleanup()
+    render (SignUp);
+    document.routerPush = vi.fn();
+
+    const emailInput = screen.getByLabelText("Your email");
+    await user.click(emailInput);
+    await user.keyboard("hola@gmail.com");
+
+    const passwordInput = screen.getByLabelText("Your password");
+    await user.click(passwordInput);
+    await user.keyboard("134");
+
+    const submitButton = screen.getByText("Signup");
+    await user.click(submitButton);
+    
+    await waitFor(() =>{
+      expect(screen.getByText("The password is too short")).toBeTruthy();
+    })
+  })
+})
